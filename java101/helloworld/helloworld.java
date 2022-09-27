8544c612d4bb69121d072e797eb80407baa9dd54
martinknu <martinknu@gmail.com>
2022-09-27 14:46:00 +0300

    first commit

---
+++ java101/helloworld/helloworld.java
@@ -0,0 +1,13 @@
+ class A {
+ public static void main(String args[]){
+    String name = "Martin";
+    int age = 40;
+    float weight = 85.5f;
+    char firstLetter = 'M';
+    boolean male = true;
+
+    String nameUpper = name.toUpperCase(); 
+
+    System.out.println("My name is " + nameUpper  + " my age is " + age + " I weigh " + weight + " The first letter in my name is " + firstLetter + " and it is " + male + " that i am male of gender" );
+ }
+}
